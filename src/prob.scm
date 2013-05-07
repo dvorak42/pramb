39c08b444a9de4b47ae0c98bde3f414be16e401c
Steven Valdez <dvorak42@mit.edu>
2013-05-06 20:40:45 -0400

    Added initial prob object stuff

---
+++ src/prob.scm
@@ -0,0 +1,11 @@
+(define (p:sum p1 p2)
+	(lambda (succeed fail)
+		(p1 (lambda (v1)
+					(p2 (lambda (v2) (succeed (+ v1 v2))) fail)) fail)))
+
+(define (p:mult p1 p2)
+	(lambda (succeed fail)
+		(p1 (lambda (v1)
+					(p2 (lambda (v2) (succeed (* v1 v2))) fail)) fail)))
+
+
