603ed43ff6cbc8249389c04aaa7d7b4e72ca4a5b
David Lawrence <dlaw@dlaw.me>
2013-05-10 09:56:25 -0400

    clean up embedded-amb

--- src/embedded-amb.scm
+++ src/embedded-amb.scm
@@ -1,14 +1,28 @@
 ;;; This file should be run in the embedded interpreter.
 
+(define (fail)
+  (ambc
+   (lambda (succeed fail)
+     (fail))))
+
+(define (amb . options)
+  (ambc
+   (lambda (succeed fail)
+     (if (null? options)
+	 (fail)
+	 (let ((result (car options)))
+	   (set! options (cdr options))
+	   (succeed result))))))
+
 (define (amb-range low high)
   (ambc
    (lambda (succeed fail)
      (succeed (+ low (random (exact->inexact (- high low))))))))
 
-(define (amb . options)
-  (ambc (lambda (s f)
-	  (if (null? options)
-	      (f)
-	      (let ((result (car options)))
-		(set! options (cdr options))
-		(s result))))))
+(define (amb-unit-sphere)
+  (let ((x (amb-range -1 1))
+	(y (amb-range -1 1))
+	(z (amb-range -1 1)))
+    (if (> (+ (square x) (square y) (square z)) 1) (fail))
+    (list x y z)))
+
