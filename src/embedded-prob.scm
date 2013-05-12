061d3fc1a814a2d0e0129a0ff6a4bd888ec73899
Jacob Hurwitz <jhurwitz@mit.edu>
2013-05-12 01:32:46 -0400

    Fix typo s/psuedo/pseudo/
    Also, I think my editor changed tabs->spaces when I saved

--- src/embedded-prob.scm
+++ src/embedded-prob.scm
@@ -10,12 +10,12 @@
           (p2 (lambda (v2) (succeed (* v1 v2))) fail)) fail)))
 
 (define p:uniform
-	(lambda (s f) (s (random 1.0))))
+  (lambda (s f) (s (random 1.0))))
 
-(define (p:psuedonormal d)
-	(if (= d 0)
-			(p:sum p:uniform p:uniform)
-			(p:sum (p:psuedonormal (- d 1)) (p:psuedonormal (- d 1)))))
+(define (p:pseudonormal d)
+  (if (= d 0)
+      (p:sum p:uniform p:uniform)
+      (p:sum (p:pseudonormal (- d 1)) (p:pseudonormal (- d 1)))))
 
 (define p:normal
-	(p:psuedonormal 2))
+  (p:pseudonormal 2))
