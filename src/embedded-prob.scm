ccd0bca579148b089e9b47abcd6aebdd3cf91bbb
Steven Valdez <dvorak42@mit.edu>
2013-05-12 01:21:28 -0400

    Added basic prob distributions

--- src/embedded-prob.scm
+++ src/embedded-prob.scm
@@ -9,3 +9,13 @@
     (p1 (lambda (v1)
           (p2 (lambda (v2) (succeed (* v1 v2))) fail)) fail)))
 
+(define p:uniform
+	(lambda (s f) (s (random 1.0))))
+
+(define (p:psuedonormal d)
+	(if (= d 0)
+			(p:sum p:uniform p:uniform)
+			(p:sum (p:psuedonormal (- d 1)) (p:psuedonormal (- d 1)))))
+
+(define p:normal
+	(p:psuedonormal 2))
